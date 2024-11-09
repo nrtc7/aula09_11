7f42c904a5170cc04ee12a3662bc89ce07a81a95
nrtc7 <nathanrtc@icloud.com>
2024-11-09 11:11:21 -0300

    Add files via upload

---
+++ nrtc_project/nrtc_project/main.swift
@@ -0,0 +1,36 @@
+// Banco
+// Operações:
+// Depósito
+// Saque
+// Ver o saldo
+
+var balance : Double = 0
+
+///
+///// 1) Ver saldo. Deve mostrar a seguinte mensagem:
+//print("Saldo atual: \(balance)")
+/////
+///// 2) depósito. Adicionar à conta o valor digitado pelo usuário.
+//if let depositValueSring = readLine() {
+//    if let depositValue = Double(depositValueSring) {
+//        balance += depositValue
+//    }
+//}
+////("Saldo atual: \(balance)")
+//
+//
+//print("Saldo atual: \(balance)")
+///// 3) Saque. Subtrair da conta o valor digitado pelo usuário
+//if let withdrawValueSring = readLine() {
+//    if let withdrawValue = Double(withdrawValueSring) {
+//        balance -= withdrawValue
+//    }
+//}
+//print("Saldo atual: \(balance)")
+
+//4) Fazer o menu
+// 1. Ver o saldo
+// 2. Depositar
+// 3. Saque
+// 0. Sair
+
